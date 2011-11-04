3ae2d84293cdf6629fcd105dc22199435dad5476
chickentuna <chickentuna@hotmail.com>
2011-11-04 14:32:15 +0100

    Update Population/src/pop/Entity.java

--- Population/src/pop/Entity.java
+++ Population/src/pop/Entity.java
@@ -5,4 +5,18 @@ import java.awt.Rectangle;
 public class Entity {
 	protected int x,y;
 	protected Rectangle bounds;
+	
+	public Entity(int x, int y) {
+		this.x=x;
+		this.y=y;
+		bounds = new Rectangle(x,y,1,1);
+	}
+	
+	public void collidesWith(Entity other) {
+		return bounds.intersect(other.getBounds());
+	}
+	
+	public Rectangle getBounds() {
+		return bounds;
+	}
 }
