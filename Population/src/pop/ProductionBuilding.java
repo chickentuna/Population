028aedafe7c68be7abf8013b3a78a83eeb403f2d
chickentuna <chickentuna@hotmail.com>
2011-11-07 09:46:32 +0100

    completeProduction() started todo: Make a stash manager

--- Population/src/pop/ProductionBuilding.java
+++ Population/src/pop/ProductionBuilding.java
@@ -3,6 +3,7 @@ package pop;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public abstract class ProductionBuilding extends Building {
 	
@@ -26,13 +27,48 @@ public abstract class ProductionBuilding extends Building {
 		if (state == State.ACTIVE) {
 			timer+=1;
 			if (timer >= production.getProductionTime()) {
-				state = State.IDLE;
-	
+				completeProduction();
 				
 			}
 		}
 	}
 	
+	private void completeProduction() {
+		state = State.IDLE;
+		//Remove used goods from stock
+		GoodMap input = production.getInput();
+		for (Good used : input.keySet()) {
+			for (Good stocked : stock.keySet()) {
+				if (used==stocked) {
+					stock.put(used, stock.get(used) - input.get(used));
+					if (stock.get(used) <= 0)
+						stock.remove(used);
+				}
+			}
+		}
+		
+		//Delegate goods to workers
+		GoodMap output = (GoodMap) production.getOutput().clone();
+		
+		Stack<Villager> workers = new Stack<Villager>();
+		for (Position pos : workforce) {
+			for (Villager v : pos.getWorkers()) {
+				v.setState(Villager.State.IDLE);
+				workers.add(v);
+			}
+		}
+		while (!output.isEmpty() || !workers.isEmpty()) {
+			Villager v = workers.pop();
+			v.collect(output);
+		}
+		if (!output.isEmpty()) {
+			stash(output);
+		}
+		
+		
+		
+	}
+
 	public boolean startProduction() {
 		if (can()) {
 			for (Position pos : workforce) {
