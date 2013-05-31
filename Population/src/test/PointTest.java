2a0dd080b7e1317194534b4e7102bbadd663a44b
Julien <Julien@192.168.1.143>
2013-05-31 12:25:08 +0200

    manhattandistance tested

--- Population/src/test/PointTest.java
+++ Population/src/test/PointTest.java
@@ -1,6 +1,6 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import kernel.Point;
 
 import org.junit.Test;
@@ -16,12 +16,26 @@ public class PointTest {
 		Point d = new Point(-5,0);
 		
 		float delta = 0.0001f;
-		assertEquals("O to 5;5",Math.PI/4,o.directionTo(a),delta);
-		assertEquals("O to -5;-5",-3*Math.PI/4,o.directionTo(b),delta);
-		assertEquals("O to 5;-5",-Math.PI/4,o.directionTo(c),delta);
-		assertEquals("O to -5;0",Math.PI,o.directionTo(d),delta);
+		assertEquals("O to 5;5", -Math.PI / 4, o.directionTo(a), delta);
+		assertEquals("O to -5;-5", 3 * Math.PI / 4, o.directionTo(b), delta);
+		assertEquals("O to 5;-5", Math.PI / 4, o.directionTo(c), delta);
+		assertEquals("O to -5;0", -Math.PI, o.directionTo(d), delta);
 		
 		
+	}
+
+	@Test
+	public void manhattanDistanceTest() {
+		Point o = new Point(0, 0);
+		Point a = new Point(5, 5);
+		Point b = new Point(-5, -5);
+		Point c = new Point(5, 0);
+
+		float delta = 0.0001f;
+		assertEquals("O to 5;5", 10, Point.manhattanDistance(o, a));
+		assertEquals("O to -5;-5", 10, Point.manhattanDistance(o, b));
+		assertEquals("O to 5;0", 5, Point.manhattanDistance(o, c));
+
 	}
 
 }
