1979d08be3ccbdc3fcea5c4316c47a29bdbd0a83
chickentuna <chickentuna@hotmail.com>
2013-06-01 17:30:53 +0200

    spriteMapped VillageRenderer

--- Population/src/view/VillagerRenderer.java
+++ Population/src/view/VillagerRenderer.java
@@ -1,21 +1,61 @@
 package view;
 
+import java.util.HashMap;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+
 import kernel.Entity;
+import kernel.Point;
 import io.graphics.Sprite;
 import io.graphics.SpriteLoader;
+import model.VState;
 import model.Villager;
+import model.behaviour.GoingBehaviour;
 
 public class VillagerRenderer extends EntityRenderer {
 
-	public final static int IDLE = 0;
-	public final static int WALKING = 1;
-	public final static int LABOUR = 2;
-	public final static int COLLECT = 3;
+	private Villager villager;
+	private HashMap<VState, Integer> spriteMap;
 
 	public VillagerRenderer(Entity v) {
 		super(v);
 		sprite = SpriteLoader.get(Sprite.Clefairy);
+		try {
+			villager = (Villager) entity;
+		} catch (Exception e) {
+			throw new RuntimeException(
+					"Passing incorrect entity to renderer.\nEntity : "
+							+ v.getClass().getSimpleName() + "\nRenderer : "
+							+ getClass().getSimpleName());
+		}
+		spriteMap = createSpriteMap();
+
 	}
 
+	private HashMap<model.VState, Integer> createSpriteMap() {
+		HashMap<VState, Integer> res = new HashMap<>();
+		int idle = Sprite.Clefairy;
+		int walking = Sprite.Clefairy;
+		int working = Sprite.Clefairy;
+		int collecting = Sprite.Clefairy;
+
+		res.put(VState.IDLE, idle);
+		res.put(VState.COLLECTING, collecting);
+		res.put(VState.GOING, walking);
+		res.put(VState.WANDERING, walking);
+		res.put(VState.LABOURING, working);
+		return res;
+	}
+
+	@Override
+	public void render(Graphics g) {
+
+		Integer s = spriteMap.get(villager.getState());
+		if (s != null) {
+			sprite = SpriteLoader.get(s);
+		}
+		super.render(g);
+	}
 
 }
