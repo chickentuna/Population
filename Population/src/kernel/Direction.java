ae9b2d21beec45a6e3c5550ed4f0fbc69187083e
Julien <chickentuna@hotmail.com>
2014-01-16 09:55:01 +0100

    added Direction and sprites

---
+++ Population/src/kernel/Direction.java
@@ -0,0 +1,9 @@
+package kernel;
+
+public class Direction {
+	public static final int DOWN = 0;
+	public static final int LEFT = 1;
+	public static final int RIGHT = 2;
+	public static final int UP = 3;
+	
+}
